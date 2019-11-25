e86c05bef6a7d04bf21f6ac26844e53db3dd039d
student <evanlay@billericak12.com>
2019-11-25 10:49:24 -0500

    Exercise Commit

---
+++ src/com/company/exercises.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class exercises {
+    public static void main(String[] args){
+        
+    }
+
+    public int addOdds(int n) {
+        int x = 1;
+        int total = 0;
+        for (x == 1, x < n, x++) {
+            total += x;
+        }
+        return total;
+    }
+}
